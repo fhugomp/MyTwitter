c570c21201c38111b6c219feb5456455f50d1413
Hugo Mendes <fhugomp@gmail.com>
2025-02-19 09:28:55 -0300

    classe perfil criada

--- mytwitter/perfil.py
+++ mytwitter/perfil.py
@@ -6,16 +6,16 @@ from tweet import Tweet
 class Perfil:
     def __init__(self, usuario: str) -> None:
         self.__usuario = usuario
-        self.__seguidos = None
-        self.__seguidores = None
+        self.__seguidos = []
+        self.__seguidores = []
         self.__tweets = []
         self.__ativo = True
 
-    def add_seguidor(self) -> None:
-        self.__seguidores += 1
+    def add_seguidor(self, perfil: "Perfil") -> None:
+        self.__seguidores.append(perfil)
 
-    def add_seguidos(self) -> None:
-        self.__seguidos += 1
+    def add_seguidos(self, perfil: "Perfil") -> None:
+        self.__seguidos.append(perfil)
 
     def add_tweet(self, mensagem: str) -> None:
         self.__tweets.append(Tweet(self.__usuario, mensagem))
@@ -26,23 +26,31 @@ class Perfil:
             self.__tweets, key=lambda tweet: tweet.get_data_postagem(), reverse=True
         )
 
-    def get_tweet(self) -> Tweet:
-        pass
+    def get_tweet(self, id: str) -> Tweet:
+        for tweet in self.get_tweets():
+            if id == tweet.get_id():
+                return tweet
 
-    def get_date_time(self) -> datetime:
-        pass
+    def get_timeline(self) -> list:
+        timeline = []
+        timeline.extend(self.__tweets)  # Adiciona os tweets do perfil atual
 
-    def get_timeline(self):
-        pass
+        # Adiciona os tweets dos perfis seguidos
+        for perfil in self.__seguidos:
+            timeline.extend(perfil.get_tweets())
 
-    def set_usuario(self):
-        pass
+        return sorted(
+            timeline, key=lambda tweet: tweet.get_data_postagem(), reverse=True
+        )
 
-    def get_usuario(self):
-        pass
+    def set_usuario(self, novo_usuario: str) -> None:
+        self.__usuario = novo_usuario
 
-    def set_ativo(self):
-        pass
+    def get_usuario(self) -> str:
+        return self.__usuario
 
-    def get_ativo(self):
-        pass
+    def set_ativo(self, estado: bool) -> None:
+        self.__ativo = estado
+
+    def get_ativo(self) -> bool:
+        return self.__ativo
