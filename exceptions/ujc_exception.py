2c7d7c60983d4d550284f25762a66388ed267705
Hugo Mendes <fhugomp@gmail.com>
2025-02-19 14:40:12 -0300

    Crindo exceçoes

---
+++ exceptions/ujc_exception.py
@@ -0,0 +1,5 @@
+class UJCException(Exception):
+    """Exceção lançada quando um usuário já está cadastrado."""
+    def __init__(self, message="Usuário já cadastrado"):
+        self.message = message
+        super().__init__(self.message)
---
+++ exceptions/unc_exception.py
@@ -0,0 +1,5 @@
+class UNCException(Exception):
+    """Exceção lançada quando um usuário não está cadastrado."""
+    def __init__(self, message="Usuário não cadastrado"):
+        self.message = message
+        super().__init__(self.message)
